b1dae66be2a2d75bd4aa6f75ea0c98ede2b126df
Antonio Ramón Vázquez León <byminter@gmail.com>
2024-06-11 09:27:08 +0200

    Change name on master

--- include/myinclude.h
+++ include/myinclude.h
@@ -2,7 +2,7 @@
 void f()
 {
     char c1[100]= "Hello world";
-    char c2[100]= ", I am <your name here>";
+    char c2[100]= ", I am Antonio R. Vazquez";
     printf("%s\n", strcat(c1, c2));
     return 0
 }
